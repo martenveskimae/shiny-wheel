06965d1e14c4ee0ab0bf35b67be8b507822c173a
martenveskimae <marten.veskimae@gmail.com>
2017-08-07 13:11:32 +0300

    shiny-wheel

---
+++ app.R
@@ -0,0 +1,46 @@
+library(shiny)
+
+df = data.frame(deeds = c("Hug", "Smile", "Call", "Compliment", "Positive note", "Thank someone", "Donate", "Laugh", "Listen", "Say YES", "Be grateful", "Love"),
+                motivation = c("Today you should hug someone! You can make someone's day by just giving them nice warm bear hug. It can be a family member, colleague, friend or a random stranger. But remember not do hug someone you don't know without asking for permission first.",
+                               "Today you should smile to a stranger! You can brighten someone's day just by smiling at them on the street.",
+                               "Today you should call an old friend! This friend may even be your mom, granddad or sister that you haven't spoken to for a while. Make their day by showing that you remembering them!",
+                               "Today you should give a compliment! Let colleague know that their hard work is being noticed. Let the guy with the cool bike know that you like his ride. Or compliment someone online.",
+                               "Today you should leave a positive note! Leave a positive note for a family member or a friend or a stranger. Maybe hide it from the sight so it will be a suprise. ",
+                               'Today you should give thanks. Let the barista know that you appriciate his work. Say "Thank you" to your partner for being amazing.',
+                               "Today you should donate. You can donate old cloths or things. Or you can help someone with little money. Or donate your time for great cause.",
+                               "Today you should make someone laugh. Tell a joke or pun at work. Make funny impressions to kids. Or send a hilarious picture to a friend.",
+                               "Today you sould listen. Ask a friend (maybe in trouble) how are they doing and if they want to talk listen. Give your time to listen.",
+                               'Today you should say "Yes" to someone. Maybe it is friend that wants to meet. Maybe its stranger asking for help. Maybe it is your kid that wants to play with you. Say YES!',
+                               "Today you should be grateful. You should write down 3 to 5 things that you are grateful for.",
+                               "Today you should love. Let at least one person know that you love him/her and maybe even give a reason. This will make their day!"),
+                images = c("hug.jpeg", "smile.jpeg", "call.jpeg", "compliment.jpeg", "positive_note.jpeg", "thank_someone.jpeg", "donate.jpeg", "laugh.jpeg", "listen.jpeg", "say_yes.jpeg", "be_grateful.jpeg", "love.jpeg"))
+
+ui = fluidPage(
+  includeCSS("www/custom.css"),
+  tags$head(tags$script(src="http://d3js.org/d3.v4.min.js")),
+  tags$head(tags$script(src="wheel.js")),
+  
+  h2("Shiny Wheels"),
+  sliderInput("deeds.nr", "Number of deeds:", min = 1, max = 12, value = 6),
+  HTML(paste("<link href='http://fonts.googleapis.com/css?family=Courgette' rel='stylesheet' type='text/css'>
+              <button id='wheelButton' onclick='spin()'>Spin</button>
+              <div id='wheel'></div>
+              <div id='results' style='width: 700px'></div>
+              <script>init()</script>")),
+  uiOutput("spinner")
+)
+
+server = function(input, output, session){
+  
+  update = eventReactive(input$deeds.nr,{
+     filtered.df = df[1:input$deeds.nr,]
+     session$sendCustomMessage(type="jsondata", filtered.df)
+   })
+  
+  output$spinner = renderUI({
+    update()
+  })
+  
+}
+shinyApp(ui = ui, server = server)
+
---
+++ www/wheel.js
@@ -0,0 +1,192 @@
+function init(){
+	////////////////////////////////////////////////////////////
+	//////////////////////// Setup /////////////////////////////
+	////////////////////////////////////////////////////////////
+	margin = {left: 20, top: 100, right: 20, bottom: 100},
+	width = 700 - margin.left - margin.right,
+	height = 750 - margin.top - margin.bottom;
+
+	colorScale = d3.scaleOrdinal(d3.schemeCategory20c);
+
+	arc = d3.arc()
+	.innerRadius(10) 
+	.outerRadius(width*0.9/2 + 30);
+
+	pie = d3.pie()
+	.value(function(d) { return d.end - d.start; })
+	.padAngle(.005)
+	.sort(null);
+
+	svg = d3.select("#wheel")
+	.append("svg")
+	.attr("width", (width + margin.left + margin.right))
+	.attr("height", (height + margin.top + margin.bottom))
+	.append("g").attr("class", "wrapper")
+	.attr("transform", "translate(" + (width / 2 + margin.left) + "," + (height / 2 + margin.top) + ")");
+
+	g = svg.append("g")
+	.attr("class", "dharmachakra");
+
+}
+
+Shiny.addCustomMessageHandler("jsondata", function(message){
+
+	////////////////////////////////////////////////////////////
+	//////////////////////// Data //////////////////////////////
+	////////////////////////////////////////////////////////////
+
+	var dataframe = message,
+	spinData = new Array;
+
+	dataframe.deeds.forEach(function(d,i) {
+		u = 360/dataframe.deeds.length;
+		if(i==0){
+			var s = 0,
+			e = u;
+		} else {
+			var s = spinData[i-1].end +1,
+			e = s + u - 1;
+		};
+		spinData.push({label: d, start: s, end: e, motivation: dataframe.motivation[i], image:  dataframe.images[i] });
+	});
+
+	////////////////////////////////////////////////////////////
+	/////////////// Create & Update Slices /////////////////////
+	////////////////////////////////////////////////////////////
+
+	update(spinData);
+	function update(source){
+		var t = d3.transition()
+    	.duration(750);
+
+    	var arcs = g.selectAll(".sliceArc")
+		.data(pie(source), function(d,i) { return i; });
+
+		var text = g.selectAll(".sliceText")
+		.data(source, function(d,i){ return i; });
+
+		arcs.exit()
+    	.transition(t)
+      	.style("fill-opacity", 1e-6)
+      	.remove();
+
+      	text.exit()
+      	.transition(t)
+      	.style("fill-opacity", 1e-6)
+      	.remove();
+
+      	arcs.transition(t)
+      	.style("fill", function(d,i) { return colorScale(i); })
+		.attr("id", function(d,i) { return "sliceArc_"+i; })
+		.attr("d", arc);
+
+		text.transition(t)
+		.attr("dx", function(d,i){ return (width*0.9/2 + 30) * Math.PI / dataframe.deeds.length; } );
+
+		arcs.enter()
+		.append("path")
+		.attr("class", "sliceArc")
+		.transition(t)
+		.style("fill", function(d,i) { return colorScale(i); })
+		.attr("id", function(d,i) { return "sliceArc_"+i; })
+		.attr("d", arc);
+
+		text.enter()
+		.append("text")
+		.attr("class", "sliceText")
+		.attr("id", function(d){return d.label;})
+		.attr("dx", (width*0.9/2 + 30) * Math.PI / dataframe.deeds.length)
+		.attr("dy", 30)
+		.append("textPath")
+		.attr("class", "sliceTextPath")
+		.style("text-anchor", "middle")
+		.attr("xlink:href", function(d,i){return "#sliceArc_"+i;})
+		.text(function(d){return d.label;});
+	}
+
+	d3.selectAll(".sliceDots").remove();
+	d3.selectAll(".pointer").remove();
+
+	var arcradius = width*0.9/2 + 25,
+	circleradius = 2.2,
+	n = (Math.PI * 2 * arcradius) / (circleradius*10);
+
+	for(i=0; i<n; i++){
+		var ang = (Math.PI * 2 * i) / n,
+		cx = arcradius * Math.sin(ang),
+		cy = arcradius * Math.cos(ang);
+
+		g.append("circle")
+		.attr("class", "sliceDots")
+		.attr('cx', cx)
+		.attr('cy', cy)
+		.attr('r', circleradius)
+		.attr("fill", "white");
+	};
+
+	svg.append("polygon")
+	.attr("class", "pointer")
+	.attr("points", (-(width*0.9/2 + 30)/24) + "," +(-(width*0.9/2 + 30) - (width*0.9/2 + 30)/12) + " " +
+		((width*0.9/2 + 30)/24) + "," + (-(width*0.9/2 + 30) - (width*0.9/2 + 30)/12) + " " +
+		"0," + (-(width*0.9/2 + 30) - (width*0.9/2 + 30)/12 + 40)) 
+	.style('opacity', 1)
+	.attr("fill", "#fff7f7")
+	.attr("stroke", "#aaaaaa")
+	.attr("stroke-width", 2);           
+
+	svg.append("circle")
+	.attr("class", "pointer")
+	.attr("id", "pointerCircle")
+	.attr("cx", 0)
+	.attr("cy", -width/2-25)
+	.attr("r", (width*0.9/2 + 30)/24)
+	.attr("fill", "#fff7f7")
+	.attr("stroke", "#aaaaaa")
+	.attr("stroke-width", 2);
+
+	////////////////////////////////////////////////////////////
+	//////////////////// Spin function /////////////////////////
+	////////////////////////////////////////////////////////////
+	var pointerRect = document.getElementById("pointerCircle").getBoundingClientRect(),
+	pointerRectCenterX = (pointerRect.right + pointerRect.left) / 2,
+	pointerRectCenterY = (pointerRect.bottom + pointerRect.top) / 2;
+
+	var selectedSlice = function(){
+		topDeed = [];
+		for(i=0; i<dataframe.deeds.length; i++){
+			var sliceRect = document.getElementById("sliceArc_"+i).getBoundingClientRect(),
+			textRectCenterX = (sliceRect.right + sliceRect.left) / 2,
+			textRectCenterY = (sliceRect.bottom + sliceRect.top) / 2,
+			dist = Math.sqrt((Math.abs(pointerRectCenterX-textRectCenterX)^2) + (Math.abs(pointerRectCenterY-textRectCenterY)^2));
+
+			topDeed.push(dist);
+		}
+
+		var result = spinData[topDeed.indexOf(Math.min(...topDeed))];
+		return result;
+	};
+
+	var running = false;
+	spin = function() {
+		running=!running;
+		if(!running){spin();};
+
+		var deg = Math.floor((Math.random() * 5500) + 800),
+		duration = (deg*400)/360;
+
+		var transform = function() {
+			return d3.interpolateString("rotate(0)", "rotate(" + deg + ")");
+		};
+
+		d3.select(".dharmachakra")
+		.transition()
+		.ease(d3.easeQuadOut)
+		.duration(duration)
+		.attrTween("transform", transform);
+
+		d3.timer(function(){
+			document.getElementById("results").innerHTML = selectedSlice().label + "!";
+		}, 0);
+	};
+
+});
